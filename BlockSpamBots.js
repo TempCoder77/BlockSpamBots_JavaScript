c38d194d2494b1040c0521ac664d856da1f48d85
TempCoder77 <tempcoder77@gmail.com>
2022-08-15 22:04:07 +0200

    Updated with working script and name, email still just placeholder

--- BlockSpamBots.js
+++ BlockSpamBots.js
@@ -3,7 +3,7 @@ class BlockSpamBots {
     constructor(name, mail, text) {
         this.name = name;
         this.mail = mail;
-        this.text = text;
+        this.text = text.toLowerCase();
     }
 
     get_name() {
@@ -15,9 +15,52 @@ class BlockSpamBots {
     }
 
     get_text() {
-        return this.text;
+        console.log(this.text); // here for debuging
+        const chars = this.text.split("");
+        const ascii_chars = "abcdefghijklmnopqrstuvwxyåäö1234567890 @!&()?+".split("");
+
+        const words = this.text.split(" ");
+        const controll_words = ["sex", "nude", "xxx"];
+        let error_text = "";
+        
+        let counter = 0;
+        //checking every char in text for none ascii characters of our choise in ascii_chars list
+        for (let item = 0; item < chars.length; item++) {
+            for (let i = 0; i < ascii_chars.length; i++) {
+                //console.log("Item: " + chars[item] + " ascii: " + ascii_chars[i])
+                if (chars[item] === ascii_chars[i]) {
+                    counter++;
+                    break;
+                }                
+            }
+            if (counter == 0) {
+                return "Not a valid char text"
+            }
+            else {
+                counter = 0
+            }
+        }
+        //Here we match a predefined list with input text and have to switch our controller if statement so it fit.
+        // We could use a shorthand code here but we are going to store words in error_text to ilustrate the errors on the input.
+        //words.some(item => controll_words.include(item)) // this will return true if it's a match else false.
+        for (let item = 0; item < words.length; item++) {
+            for (let i = 0; i < controll_words.length; i++) {
+                if (words[item] === controll_words[i]) {
+                    counter++;
+                    break;
+                }                
+            }
+            if (counter == 0) {
+                counter = 0
+            }
+            else {
+                return "Not a valid word text"
+            }
+        }
+
+        return true;
     }
 }
 
-const run_block = new BlockSpamBots("Micke", "text@test.org", "Spam text to send");
-console.log(run_block.get_name());
+const run_block = new BlockSpamBots("Micke", "text@test.org", "måste köpas sex");
+console.log(run_block.get_text());
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 <h1> Seting up project block spam bot for javascript</h1>
+<p></p>
